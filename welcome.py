fcdf8445213629b4b5116af2097fb69100ee4e0b
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2017-03-19 13:48:01 +0530

    Removed @wizards mention from message because bird wanted to nap

--- welcome.py
+++ welcome.py
@@ -2,7 +2,7 @@
 
 import argparse
 
-text = "@{user} I see you're new around here. Welcome! Make sure you read #welcome and message @wizards for problems, and fear their awesome moderator powers. Know, however, that I am the true power holder of this place, but do keep that a secret."
+text = "@{user} I see you're new around here. Welcome! Make sure you read #welcome and message wizards for problems, and fear their awesome moderator powers. Know, however, that I am the true power holder of this place, but do keep that a secret."
 
 
 def main():
@@ -10,7 +10,6 @@ def main():
     parser.add_argument('user', help='The name of the user to be welcomed.')
 
     args = parser.parse_args()
-    print (args)
     print (text.format(user=args.user))
 
 
